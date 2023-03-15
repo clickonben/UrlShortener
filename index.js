ef7d99e94b234ba3b2b6277aab7abe078a51c108
Ben Robinson <ben@click-on.co.uk>
2023-03-15 18:45:37 +0000

    completed course and modified server.js so it actually works

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 require('dotenv').config();
-const express = require('express');
+var express = require('express');
+var app = express();
+const https = require('https');
+const fs = require('fs');
 const cors = require('cors');
-const app = express();
+const dns = require('node:dns');
+const util = require('util');
+let bodyParser = require('body-parser');
+mongoose = require("mongoose");
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const dnsLookup = util.promisify(dns.lookup);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then (() => {
+  console.log("connected to database succesfully")
+})
+.catch(() => {
+  console.log("failed to connect to database")
+});
+
+const UrlMapSchema = new mongoose.Schema({
+  baseUrl: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+let UrlMap = mongoose.model("UrlMap", UrlMapSchema);
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -14,11 +39,30 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async (req, res) => {  
+  try {
+    const url = new URL(req.body.url);
+    await dnsLookup(url.hostname);
+    let urlMap = await UrlMap.findOne({baseUrl: url});
+    if (urlMap === null) {
+      urlMap = new UrlMap ({ baseUrl: req.body.url })
+      await urlMap.save();
+    }
+    res.json({ original_url: urlMap.baseUrl, short_url: urlMap.id});      
+  } catch (error) {
+    res.json({ error: 'invalid url' });
+  } 
+});
+app.get('/api/shorturl/:id', async (req, res) => {
+  let urlMap = await UrlMap.findById(req.params.id);
+  res.redirect(urlMap.baseUrl);
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+const options = {
+  key: fs.readFileSync(process.env.SSL_KEY_PATH),
+  cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+}
+
+const PORT = process.env.PORT || 443
+const listener = https.createServer(options, app).listen(PORT, console.log(`Node.js listening on port  ${PORT}`))
+
